cce484304f484a59d9ac3c0de04ec51cd91efdbe
mehmet turkoz <mturkozz42@gmail.com>
2022-11-20 11:10:26 +0300

    initial commit

--- src/main/java/com/bt_akademi/product_management/MicroserviceProductBtAkademiApplication.java
+++ src/main/java/com/bt_akademi/product_management/MicroserviceProductBtAkademiApplication.java
@@ -3,8 +3,10 @@ package com.bt_akademi.product_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@PropertySource("claspath:application-${spring.profiles.active:default}.properties")
 public class MicroserviceProductBtAkademiApplication {
 
 	public static void main(String[] args) {
